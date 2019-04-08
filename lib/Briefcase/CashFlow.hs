48bbb7543b2c3b9b07cc25dda53c0ecce2e94287
Andrew Cowie <andrew@operationaldynamics.com>
2019-04-08 23:42:17 +1000

    Include seed date first when incrementing

--- lib/Briefcase/CashFlow.hs
+++ lib/Briefcase/CashFlow.hs
@@ -12,10 +12,14 @@ monthly = increment (Period 0 1 0)
 
 fortnightly :: Date -> [Date]
 fortnightly = increment (Period 0 0 14)
-    
+
+{-|
+Return an infinite lists of dates incrementing by the period specified,
+starting from (and including) the given seed date.
+-}
 increment :: Period -> Date -> [Date]
 increment delta from =
   let
     next = dateAddPeriod from delta
   in
-    next : increment delta next
+    from : increment delta next
